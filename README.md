cebaf6b355da9a765b017e58b4a9e3f27532b5fb
pacosalces <pacosalces@gmail.com>
2020-09-17 10:07:28 -0700

    first commit for physunits pkg repo

---
+++ README.md
@@ -0,0 +1,15 @@
+# physunits
+
+Simple package with common physical unit definitions. 
+
+## Installation
+
+## Usage
+
+An example usage of this package is:
+``from physunits import *``
+which updates the globals dict with all ``physunits`` definitions. If only a set of units is desired, manually importing them as:
+``from physunits import mm, nm, um``
+is sufficient. 
+
+## Contatct and support
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="physunits-pkg-pacosalces", # Replace with your own username
+    version="0.0.1",
+    author="pacosalces",
+    author_email="pacosalces@gmail.com",
+    description="Physical units as global variables for simple numerical propagation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pacosalces/physunits",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
