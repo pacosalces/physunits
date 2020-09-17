ca396f6ca1aaab1cadb743a7ca1975a6907d2182
pacosalces <pacosalces@gmail.com>
2020-09-17 11:20:21 -0700

    Add some other common units and prefixes.

--- physunits/__init__.py
+++ physunits/__init__.py
@@ -18,8 +18,11 @@ import numpy as np
 
 
 # length
-m, mm, um, nm = np.logspace(0, -9, 4)
+km, m, mm, um, nm = np.logspace(3, -9, 5)
 cm = m/100
+
+# mass (notice the SI convention for the base unit)
+ng, ug, mg, g, kg, = np.logspace(-6, 1, 5)
 
 # frequency
 mHz, Hz, kHz, MHz, GHz, THz = np.logspace(-3, 12, 6)
@@ -33,6 +36,14 @@ nW, uW, mW, W, kW, MW = np.logspace(-9, 6, 6)
 # temperature
 nK, uK, mK, K = np.logspace(-3, 0, 4)
 
+# current
+nA, uA, mA, A = np.logspace(-9, 0, 4)
+
+# voltage
+pV, nV, uV, mV, V, kV = np.logspace(-12, 3, 6)
+
+# energy
+mJ, J, kJ = np.logspace(-3, 3, 3)
 
 ### ###  ### ##   ### ###  ### ###  ### ##    ## ##   ##   ##
  ##  ##   ##  ##   ##  ##   ##  ##   ##  ##  ##   ##   ## ##
@@ -54,3 +65,6 @@ nK, uK, mK, K = np.logspace(-3, 0, 4)
 # length
 inch = 25.4 * mm
 ft = 12 * inch
+yd = 3 * ft
+
+# power
