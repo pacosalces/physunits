6266a4660bf596323ee5c848766b753a6535eb21
pacosalces <pacosalces@gmail.com>
2020-09-19 09:57:49 -0700

    Add angle, pressure, new imperial mass and pressure units, a new section of alternative or "OTHER" units, and specified some of the derived unit expansions.

--- physunits/__init__.py
+++ physunits/__init__.py
@@ -8,20 +8,16 @@ import numpy as np
 ##   ##     ##
  ## ##     ####
 
-##  ###  ###  ##    ####  #### ##   ## ##
-##   ##    ## ##     ##   # ## ##  ##   ##
-##   ##   # ## #     ##     ##     ####
-##   ##   ## ##      ##     ##      #####
-##   ##   ##  ##     ##     ##         ###
-##   ##   ##  ##     ##     ##     ##   ##
- ## ##   ###  ##    ####   ####     ## ##
-
-
 # length
 km, m, mm, um, nm = np.logspace(3, -9, 5)
 cm = m/100
 
-# mass (notice the SI convention for the base unit)
+# angle (technically not SI... why not? because politics?)
+deg = 1.0
+rad = 180 * deg / np.pi
+mrad = 1e-3 * rad
+
+# mass (note the SI convention for base unit)
 ng, ug, mg, g, kg, = np.logspace(-6, 1, 5)
 
 # frequency
@@ -29,9 +25,6 @@ mHz, Hz, kHz, MHz, GHz, THz = np.logspace(-3, 12, 6)
 
 # time
 s, ms, us, ns, ps = np.logspace(0, -12, 5)
-
-# power
-nW, uW, mW, W, kW, MW = np.logspace(-9, 6, 6)
 
 # temperature
 nK, uK, mK, K = np.logspace(-3, 0, 4)
@@ -43,7 +36,13 @@ nA, uA, mA, A = np.logspace(-9, 0, 4)
 pV, nV, uV, mV, V, kV = np.logspace(-12, 3, 6)
 
 # energy
-mJ, J, kJ = np.logspace(-3, 3, 3)
+mJ, J, kJ = np.logspace(-3, 3, 3) * (kg * m**2 / s**2)
+
+# power
+nW, uW, mW, W, kW, MW = np.logspace(-9, 6, 6) * (J/s)
+
+# pressure
+mPa, Pa, kPa, MPa = np.logspace(-3, 6, 4) * (kg / m / s**2)
 
 ### ###  ### ##   ### ###  ### ###  ### ##    ## ##   ##   ##
  ##  ##   ##  ##   ##  ##   ##  ##   ##  ##  ##   ##   ## ##
@@ -67,4 +66,25 @@ inch = 25.4 * mm
 ft = 12 * inch
 yd = 3 * ft
 
-# power
+# mass
+lb = 0.45359237*kg
+oz = lb / 16
+
+# pressure
+psi = 6894.757293168*Pa
+
+ ## ##   #### ##  ###  ##  ### ###  ### ##
+##   ##  # ## ##   ##  ##   ##  ##   ##  ##
+##   ##    ##      ##  ##   ##       ##  ##
+##   ##    ##      ## ###   ## ##    ## ##
+##   ##    ##      ##  ##   ##       ## ##
+##   ##    ##      ##  ##   ##  ##   ##  ##
+ ## ##    ####    ###  ##  ### ###  #### ##
+
+# relative concentration
+ppm, ppb = 1e-6, 1e-9
+
+# pressure
+atm = 101325*Pa
+Torr = 1 / (760*atm)
+Bar, mBar = 1e-5*Pa, 1e-5*mPa
